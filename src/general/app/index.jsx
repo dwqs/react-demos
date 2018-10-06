30bf92c53f01bcb384aebd6d2d620d715876a0fe
dwqs <461147874@qq.com>
2018-10-07 01:18:39 +0800

    refactor: Add virtual list demo

--- src/general/app/index.jsx
+++ src/general/app/index.jsx
@@ -18,6 +18,8 @@ export default class App extends React.Component {
         <Link to='/react-demos/list'>An online demo to compare different impletation of virtual list</Link>(比较不同虚拟列表实现的 demo)
         <br />
         <Link to='/react-demos/dom'>DOMs</Link>(创建 DOM 元素的测试)
+        <br />
+        <Link to='/react-demos/virtual'>The virtual list whose item has the same height</Link>(列表项等高的虚拟列表 demo)
       </div>
     )
   }
--- src/page/routes.js
+++ src/page/routes.js
@@ -18,12 +18,14 @@ const createContainer = loader => Loadable({
 const App = () => import(/* webpackChunkName: "main" */ '../general/app/index')
 const List = () => import(/* webpackChunkName: "list" */ '../general/list/index')
 const Dom = () => import(/* webpackChunkName: "dom" */ '../general/dom/index')
+const Virtual = () => import(/* webpackChunkName: "virtual" */ '../general/virtual-list/index')
 
 const routes =
   <Switch>
     <Route exact path='/react-demos' component={createContainer(App)} />
     <Route path='/react-demos/list' component={createContainer(List)} />
     <Route path='/react-demos/dom' component={createContainer(Dom)} />
+    <Route path='/react-demos/virtual' component={createContainer(Virtual)} />
   </Switch>
 
 export default routes
