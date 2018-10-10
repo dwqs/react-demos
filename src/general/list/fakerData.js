190db1c64f7750a79a0fd2f7426708718fce200e
dwqs <461147874@qq.com>
2018-10-10 18:52:02 +0800

    refactor: Cache data and add useImage parameter

--- src/general/list/fakerData.js
+++ src/general/list/fakerData.js
@@ -3,7 +3,11 @@ import faker from 'faker/locale/zh_CN'
 const pageSize = 200
 const host = 'http://picsum.photos'
 
-function fakerData (start = 0) {
+const words = []
+const paragraphs = []
+const images = []
+
+function fakerData (start = 0, useImage = true) {
   const a = []
   for (let i = start; i < start + pageSize; i++) {
     const rw = (1 + Math.random()) * 100
@@ -11,9 +15,9 @@ function fakerData (start = 0) {
 
     a.push({
       id: i,
-      image: `${host}/${Math.trunc(rw)}/${Math.trunc(rh)}`,
-      words: faker.lorem.words(),
-      paragraphs: faker.lorem.sentences()
+      image: useImage ? images[i] || (images[i] = `${host}/${Math.trunc(rw)}/${Math.trunc(rh)}`) : undefined,
+      words: words[i] || (words[i] = faker.lorem.words()),
+      paragraphs: paragraphs[i] || (paragraphs[i] = Math.random() <= 0.5 ? faker.lorem.paragraphs() : faker.lorem.sentences())
     })
   }
 
